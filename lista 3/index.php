e4dd0e3d403a3723970197e403dbe4f633b64bba
JaWitold <witekkaras00@gmail.com>
2022-04-07 13:46:41 +0200

    string, almost works

--- lista 3/index.php
+++ lista 3/index.php
@@ -9,7 +9,7 @@ $mode = $argv[1];
 $fileNameIn = $argv[2];
 $fileNameOut = $argv[3];
 
-//runLZ77($fileNameIn, $mode == "encode", $fileNameOut);
+runLZ77($fileNameIn, $mode == "encode", $fileNameOut);
 runLZ77($fileNameOut, $mode != "encode", "decoded.txt");
 
 function runLZ77(string $fileName, bool $encode, string $outfile): void
@@ -25,7 +25,8 @@ function runLZ77(string $fileName, bool $encode, string $outfile): void
     $result = $encode ? $lz77->encode($file) : $lz77->decode($file);
     $end = microtime(true);
     file_put_contents($outfile, $result);
-    print_r("result: " . $result . "\n");
+//    print_r("result: " . $result . "\n");
+    print_r("===== " . ($encode ? "ENCODE" : "DECODE") . " =====\n");
     print_r("result length: " . strlen($result). " oryginal length: " . count($file) . "\n");
-    print_r(($encode ? "encode" : "decode") . " time: " . $end - $start . "\n");
+    print_r(($encode ? "encode" : "decode") . " time: " . $end - $start . "\n\n");
 }
