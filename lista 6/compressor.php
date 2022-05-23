cec973a1a90de43b5cd076d27929765a89bbf72a
JaWitold <witekkaras00@gmail.com>
2022-05-23 10:26:06 +0200

    finnito

--- lista 6/compressor.php
+++ lista 6/compressor.php
@@ -13,18 +13,6 @@ class compressor
         return $imploded;
     }
 
-    public static function bitmapToSave(array $bitmap): string
-    {
-        $imploded = "";
-        foreach ($bitmap as $b) {
-//            $imploded .= implode(array_map(function($item) {return chr($item);}, $b));
-            $imploded .= implode(array_map(function ($item) {
-                return "\n" . $item;
-            }, $b));
-        }
-        return $imploded;
-    }
-
     public static function parseBitmap(array $file, int $width, int $height): array
     {
         $bitmap = [];
@@ -66,13 +54,6 @@ class compressor
     public static function uniformQuantization(array $bitmap, int $k): array
     {
         $quan = [];
-//        $delta = 2 * (255 / pow(2, $k));
-//        foreach ($bitmap as $pixel) {
-//            $quan[] = array_map(function ($item) use ($delta) {
-//                return round($delta * (ceil($item / $delta)));
-//            }, $pixel);
-//        }
-
         $delta = (256 / pow(2, $k));
         foreach ($bitmap as $pixel) {
             $quan[] = array_map(function ($item) use ($delta) {
@@ -80,5 +61,37 @@ class compressor
             }, $pixel);
         }
         return $quan;
+    }
+
+    public static function mse(array $original, array $new): float|int
+    {
+        return array_sum(array_map(function ($index) use ($original, $new) {
+            return self::mse_i($original, $new, $index);
+        }, range(0, 2)));
+    }
+
+    public static function mse_i(array $original, array $new, int $index): float|int
+    {
+        return array_sum(array_map(function ($item) use ($original, $new, $index) {
+                return pow($original[$item][$index] - $new[$item][$index], 2);
+            }, range(0, count($original) - 1))) / count($original);
+    }
+
+    public static function power(array $x): float|int
+    {
+        $sum = 0;
+        foreach ($x as $i) {
+            $sum += $i * $i;
+        }
+        return $sum;
+    }
+
+    public static function snr(array $x, float|int $mserr): float|int
+    {
+        $sum = 0;
+        for ($i = 0; $i < count($x); $i++) {
+            $sum += self::power($x[$i]);
+        }
+        return $sum / count($x) / $mserr;
     }
 }
--- lista 6/index.php
+++ lista 6/index.php
@@ -13,6 +13,7 @@ $k = $argv[3] ?? 0;
 
 runCompressor($fileNameIn, $fileNameOut, $k, true);
 runCompressor($fileNameOut, $fileNameIn, $k, false);
+stats($fileNameIn, "_" . basename($fileNameIn));
 
 
 function runCompressor(string $fileNameIn, string $fileNameOut, int $k = 7, bool $encode = true): void
@@ -20,7 +21,7 @@ function runCompressor(string $fileNameIn, string $fileNameOut, int $k = 7, bool
     if ($encode) {
         encode($fileNameIn, $fileNameOut, $k);
     } else {
-        decode($fileNameIn, "_" . $fileNameOut);
+        decode($fileNameIn, "_" . basename($fileNameOut));
     }
 }
 
@@ -33,28 +34,49 @@ function encode(string $fileNameIn, string $fileNameOut, int $k = 7): void
     $header = implode(array_map(function ($x) {return chr($x);}, array_slice($file, 0, 18)));
     $footer = implode(array_map(function ($x) {return chr($x);}, array_slice($file, 18 + 3 * $width * $height)));
 
-//    $bitmap = [[2, 0, 0], [2, 0, 0], [250, 250, 250], [2, 0, 0]];
-
     $quantized = compressor::uniformQuantization($bitmap, $k);
     $differences = compressor::differentialCoding($bitmap, $quantized);
     $result = compressor::uniformQuantization($differences, $k);
 
     $payload = compressor::bitmapToBytes($result);
-    file_put_contents("out.tga", $header . $payload . $footer);
+    file_put_contents($fileNameOut, $header . $payload . $footer);
 
-    $decoded = compressor::differentialDecoding($result);
-
-    $payload = compressor::bitmapToBytes($decoded);
-    file_put_contents("out_d.tga", $header . $payload . $footer);
-
-    print_r($result);
-    print_r($decoded);
+//    $decoded = compressor::differentialDecoding($result);
+//
+//    $payload = compressor::bitmapToBytes($decoded);
+//    file_put_contents("out_d.tga", $header . $payload . $footer);
 
 }
 
 function decode(string $fileNameIn, string $fileNameOut): void
 {
+    $file = readTheFile($fileNameIn);
+    $width = $file[14] * 256 + $file[13];
+    $height = $file[16] * 256 + $file[15];
+    $bitmap = compressor::parseBitmap(array_slice($file, 18, 3 * $width * $height), $width, $height);
+    $header = implode(array_map(function ($x) {return chr($x);}, array_slice($file, 0, 18)));
+    $footer = implode(array_map(function ($x) {return chr($x);}, array_slice($file, 18 + 3 * $width * $height)));
 
+    $decoded = compressor::differentialDecoding($bitmap);
+
+    $payload = compressor::bitmapToBytes($decoded);
+    file_put_contents($fileNameOut, $header . $payload . $footer);
+}
+
+function stats(string $original, string $new) : void
+{
+    $bitmap = getBitmap($original);
+    $decoded = getBitmap($new);
+
+    $mse = compressor::mse($bitmap, $decoded);
+    $snr = compressor::snr($bitmap, $mse);
+
+    print_r("mse: \t\t\t\t" . $mse . "\n");
+    print_r("red channel mse: \t". compressor::mse_i($bitmap, $decoded, 2) . "\n");
+    print_r("green channel mse: \t". compressor::mse_i($bitmap, $decoded, 1) . "\n");
+    print_r("blue channel mse: \t". compressor::mse_i($bitmap, $decoded, 0) . "\n");
+
+    print_r("snr: \t\t\t\t". $snr . "\n");
 }
 
 function readTheFile(string $fileName) : array {
@@ -63,4 +85,11 @@ function readTheFile(string $fileName) : array {
     $file = fread($ff, $fileSize);
     fclose($ff);
     return unpack(sprintf('C%d', $fileSize), $file);
+}
+
+function getBitmap(string $fileName) : array {
+    $newFile = readTheFile($fileName);
+    $newWidth = $newFile[14] * 256 + $newFile[13];
+    $newHeight = $newFile[16] * 256 + $newFile[15];
+    return compressor::parseBitmap(array_slice($newFile, 18, 3 * $newWidth * $newHeight), $newWidth, $newHeight);
 }
